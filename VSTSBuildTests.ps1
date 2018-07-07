ff612156deed7cba9f20c751a32257a2758552d0
jsclifford <37558619+jsclifford@users.noreply.github.com>
2018-07-07 13:26:41 +0200

    Initial creation.

---
+++ VSTSBuildTests.ps1
@@ -0,0 +1,16 @@
+Write-Host "Checking environment variables for VSTS Builds."
+Write-Host "Working Directory"
+$env:workingdirectory
+
+Write-Host "Common.TestResultsDirectory"
+$env:COMMON_TESTRESULTSDIRECTORY
+
+
+Write-Host "Build.StagingDirectory"
+$env:BUILD_STAGINGDIRECTORY
+
+Write-Host "Build.SourcesDirectory"
+$env:BUILD_SOURCESDIRECTORY
+
+Write-Host "System.DefaultWorkingDirectory"
+$env:SYSTEM_DEFAULTWORKINGDIRECTORY
