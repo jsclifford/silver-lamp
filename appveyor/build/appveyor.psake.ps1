eb7793f4b1c4b8ed9663f6ca3051a7a95fefad02
Josh Clifford <josh@kazi.lan>
2018-09-04 22:17:49 +0200

    Updates

--- appveyor/build/appveyor.psake.ps1
+++ appveyor/build/appveyor.psake.ps1
@@ -1,21 +1,34 @@
 Import-Module PSake
 
-$buildRoot = "$PSScriptRoot\build"
+$buildRoot = "$PSScriptRoot"
 if($env:default_tests -eq 'y'){
     # Builds the module by invoking psake on the build.psake.ps1 script.
     Write-Verbose "Running Psake task TestDefault" -Verbose
-    Invoke-psake -Path "$buildRoot\build.psake.ps1" -taskList TestDefault
+    Invoke-psake -buildFile "$buildRoot\build.psake.ps1" -taskList "TestDefault"
 }else{
     # Builds the module by invoking psake on the build.psake.ps1 script.
     Write-Verbose "Running Psake task Test" -Verbose
-    Invoke-psake -Path "$buildRoot\build.psake.ps1" -taskList Test
+    Invoke-psake -buildFile "$buildRoot\build.psake.ps1" -taskList "Test"
 }
 
 
 if ($psake.build_success -eq $false){
-    Add-AppveyorMessage -Message "Unit Test Failed"
-    Update-AppveyorTest -Name "PSake Unitest" -Outcome Failed -ErrorMessage $psake.error_message -Framework NUnit
+    $error_message = @"
+Unit Test Failed. Error: 
+$($psake.error_message)   
+"@
+    if($null -ne $env:APPVEYOR_BUILD_FOLDER){
+        Add-AppveyorMessage -Message $error_message
+        Update-AppveyorTest -Name "PSake Unitest" -Outcome Failed -ErrorMessage $psake.error_message -Framework NUnit
+        
+    }else{
+        Write-Host "Psake Task failed. $($error_message)"
+    }
     throw "Psake Build Failed"
 }else{
-    Update-AppveyorTest -Name "PSake Unitest" -Outcome Passed -Framework NUnit
+    if($null -ne $env:APPVEYOR_BUILD_FOLDER){
+        Update-AppveyorTest -Name "PSake Unitest" -Outcome Passed -Framework NUnit
+    }else{
+        Write-Host "Psake Unit Test Passed."
+    }
 }
