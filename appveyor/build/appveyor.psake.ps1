7777ef4d1742743c18b1bc7e357bb560ae3a9fa3
Josh Clifford <josh@kazi.lan>
2018-09-04 22:41:59 +0200

    another try.

--- appveyor/build/appveyor.psake.ps1
+++ appveyor/build/appveyor.psake.ps1
@@ -3,7 +3,7 @@ Import-Module PSake
 if($env:default_tests -eq 'y'){
     # Builds the module by invoking psake on the build.psake.ps1 script.
     Invoke-Pester -Script "$(Split-Path $PSScriptRoot -Parent )\test"
-    Write-Verbose "Running Psake task TestDefault" -Verbose
+    Write-Verbose "Running Psake task TestDefault.  Psake script: $PSScriptRoot\build.psake.ps1" -Verbose
     Invoke-psake -buildFile "$PSScriptRoot\build.psake.ps1" -taskList "TestDefault"
 }else{
     # Builds the module by invoking psake on the build.psake.ps1 script.
@@ -16,7 +16,6 @@ if ($psake.build_success -eq $false){
     if($null -ne $env:APPVEYOR_BUILD_FOLDER){
         Add-AppveyorMessage -Message "Unit Test Failed."
         Update-AppveyorTest -Name "PSake Unitest" -Outcome Failed -ErrorMessage $psake.error_message -Framework NUnit
-        
     }else{
         Write-Host "Psake Task failed. Unit Test Failed."
     }
