f83a0421ec1efe5e19a253621d7e26790c39be4d
Josh Clifford <josh@kazi.lan>
2018-09-05 00:19:25 +0200

    running build task.

--- appveyor/build/appveyor.psake.ps1
+++ appveyor/build/appveyor.psake.ps1
@@ -3,11 +3,11 @@ Import-Module PSake
 if($env:default_tests -eq 'y'){
     # Builds the module by invoking psake on the build.psake.ps1 script.
     Write-Verbose "Running Psake task TestDefault.  Psake script: $PSScriptRoot\build.psake.ps1" -Verbose
-    Invoke-Psake "$PSScriptRoot\build.psake.1.ps1" -taskList StageFiles,TestDefault
+    Invoke-Psake "$PSScriptRoot\build.psake.1.ps1" -taskList Build,TestDefault
 }else{
     # Builds the module by invoking psake on the build.psake.ps1 script.
     Write-Verbose "Running Psake task Test" -Verbose
-    Invoke-Psake "$PSScriptRoot\build.psake.1.ps1" -taskList StageFiles,Test
+    Invoke-Psake "$PSScriptRoot\build.psake.1.ps1" -taskList Build,Test
 }
 
 
